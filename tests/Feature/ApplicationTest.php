fd70d4cebd9df9fafc042a66eaca9c9a45d0e349
Providence ifeosame <providenceifeosame@gmail.com>
2022-04-26 18:50:57 +0100

    add test for database refresh

--- tests/Feature/ApplicationTest.php
+++ tests/Feature/ApplicationTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class ApplicationTest extends TestCase
 {
     /**
      * A basic test example.
@@ -15,6 +15,13 @@ class ExampleTest extends TestCase
     public function test_the_application_returns_a_successful_response()
     {
         $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_database_can_be_refreshed()
+    {
+        $response = $this->get('/api/refresh-database');
 
         $response->assertStatus(200);
     }
